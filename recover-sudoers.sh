06217dfa73d4e33f4d0c1acbbae45cb1ec8e3511
jaejun-lee <jaejun.lee@nhnsoft.com>
2021-11-29 20:29:41 +0900

    fix: 임시 비밀번호 설정 후 커맨드 실행

--- recover-sudoers.sh
+++ recover-sudoers.sh
@@ -3,13 +3,13 @@
 MD5SUM_BIN=$(which md5sum)
 
 # Backup & change file
-BASE=$(echo 'irteamsu' | sudo -kS -u irteam "$MD5SUM_BIN" /Users/irteamsu/scripts/sudoers-backup | awk '{print $1}')
-NEW=$($MD5SUM_BIN /etc/sudoers | awk '{print $1}')
+BASE=$(echo 'irteamsu' | sudo -kS -u irteamsu "$MD5SUM_BIN" /Users/irteamsu/scripts/sudoers-backup | awk '{print $1}')
+NEW=$(echo 'irteamsu' | sudo -kS -u irteamsu "$MD5SUM_BIN" /etc/sudoers | awk '{print $1}')
 
 datetime=$(date +%Y%m%d-%H%M%S)
 if [ "${BASE}" != "${NEW}" ]; then
     echo "$datetime: $0: The file has been changed. Replace it with a backup file."
-    /bin/cp -f /Users/irteamsu/scripts/sudoers-backup /etc/sudoers
+    echo 'irteamsu' | sudo -kS -u irteamsu /bin/cp -f /Users/irteamsu/scripts/sudoers-backup /etc/sudoers
 fi
 
 echo "$0: Done."
