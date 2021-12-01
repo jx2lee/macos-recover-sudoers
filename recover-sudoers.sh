178fbebceba04c11a2edd6a21a65ab56696176ed
jaejun-lee <jaejun.lee@nhnsoft.com>
2021-12-01 13:34:50 +0900

    fix: set-sudoers.sh 삭제 및 PW 없이 irteamsu 로 커맨드 가능하게 설정

--- recover-sudoers.sh
+++ recover-sudoers.sh
@@ -3,13 +3,12 @@
 MD5SUM_BIN=$(which md5sum)
 
 # Backup & change file
-BASE=$(echo 'irteamsu' | sudo -kS -u irteamsu "$MD5SUM_BIN" /Users/irteamsu/scripts/sudoers-backup | awk '{print $1}')
-NEW=$(echo 'irteamsu' | sudo -kS -u irteamsu "$MD5SUM_BIN" /etc/sudoers | awk '{print $1}')
+BASE=$(echo 'irteamsu' | sudo -S -u irteamsu "$MD5SUM_BIN" /Users/irteamsu/scripts/sudoers-backup | awk '{print $1}')
+NEW=$(echo 'irteamsu' | sudo -S -u irteamsu "$MD5SUM_BIN" /etc/sudoers | awk '{print $1}')
 
 datetime=$(date +%Y%m%d-%H%M%S)
 if [ "${BASE}" != "${NEW}" ]; then
     echo "$datetime: $0: The file has been changed. Replace it with a backup file."
-    echo 'irteamsu' | sudo -kS -u irteamsu /bin/cp -f /Users/irteamsu/scripts/sudoers-backup /etc/sudoers
+    echo 'irteamsu' | sudo -S -u irteamsu /bin/cp -f /Users/irteamsu/scripts/sudoers-backup /etc/sudoers
+    echo "$datetie: $0: Done."
 fi
-
-echo "$0: Done."
--- set-sudoers.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo 'irteamsu' | sudo -kS -u irteam bash /Users/irteamsu/scripts/recover-sudoers.sh
