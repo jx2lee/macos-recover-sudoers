e8148221a10dfe70ee7902e4921d815265e24303
jaejun-lee <jaejun.lee@nhnsoft.com>
2021-12-02 14:17:47 +0900

    irteamsu 계정으로 수행하는 커맨드 수정

--- recover-sudoers.sh
+++ recover-sudoers.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-MD5SUM_BIN=$(which md5sum)
-
 # Backup & change file
-BASE=$(echo 'irteamsu' | sudo -S -u irteamsu "$MD5SUM_BIN" /Users/irteamsu/scripts/sudoers-backup | awk '{print $1}')
-NEW=$(echo 'irteamsu' | sudo -S -u irteamsu "$MD5SUM_BIN" /etc/sudoers | awk '{print $1}')
+BASE=$(/usr/loca/bin/md5sum /Users/irteamsu/scripts/sudoers-backup | awk '{print $1}')
+NEW=$(/usr/loca/bin/md5sum /etc/sudoers | awk '{print $1}')
 
 datetime=$(date +%Y%m%d-%H%M%S)
 if [ "${BASE}" != "${NEW}" ]; then
     echo "$datetime: $0: The file has been changed. Replace it with a backup file."
-    echo 'irteamsu' | sudo -S -u irteamsu /bin/cp -f /Users/irteamsu/scripts/sudoers-backup /etc/sudoers
-    echo "$datetie: $0: Done."
+    /bin/cp -f /Users/irteamsu/scripts/sudoers-backup /etc/sudoers
+    echo "$datetime: $0: Done."
 fi
