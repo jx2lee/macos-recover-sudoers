8ca17687110fb3c4df32da9dea3250d39736d4a6
jaejun-lee <jaejun.lee@nhnsoft.com>
2021-12-06 13:02:19 +0900

    plist 수정 및 백업파일 path 수정

--- recover-sudoers.sh
+++ recover-sudoers.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Backup & change file
-BASE=$(/usr/loca/bin/md5sum /Users/irteamsu/scripts/sudoers-backup | awk '{print $1}')
-NEW=$(/usr/loca/bin/md5sum /etc/sudoers | awk '{print $1}')
+BASE=$(/usr/local/bin/md5sum /Users/irteamsu/scripts/macos-recover-sudoers/sudoers-backup | awk '{print $1}')
+NEW=$(/usr/local/bin/md5sum /etc/sudoers | awk '{print $1}')
 
 datetime=$(date +%Y%m%d-%H%M%S)
 if [ "${BASE}" != "${NEW}" ]; then
     echo "$datetime: $0: The file has been changed. Replace it with a backup file."
-    /bin/cp -f /Users/irteamsu/scripts/sudoers-backup /etc/sudoers
+    /bin/cp -f /Users/irteamsu/scripts/macos-recover-sudoers/sudoers-backup /etc/sudoers
     echo "$datetime: $0: Done."
 fi
