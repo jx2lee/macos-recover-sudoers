8454ae21b6bbb8c1e4c2a6a9bf6205a3114e5063
jaejun-lee <jaejun.lee@nhnsoft.com>
2021-12-07 09:04:04 +0900

    md5sum to diff 변경
    * 변경 이유: md5sum 은 기본 바이너리가 아니어서 brew 로 바이너리 다운로드가 필요함
    * os 기본 바이너리로 구현

--- recover-sudoers.sh
+++ recover-sudoers.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Backup & change file
-BASE=$(/usr/local/bin/md5sum /Users/irteamsu/scripts/macos-recover-sudoers/sudoers-backup | awk '{print $1}')
-NEW=$(/usr/local/bin/md5sum /etc/sudoers | awk '{print $1}')
+# Detect if a file has changed.
+BACKUP=/Users/irteamsu/scripts/macos-recover-sudoers/sudoers-backup
+NEW=/etc/sudoers
 
 datetime=$(date +%Y%m%d-%H%M%S)
-if [ "${BASE}" != "${NEW}" ]; then
+
+/usr/bin/diff $BACKUP $NEW > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
     echo "$datetime: $0: The file has been changed. Replace it with a backup file."
     /bin/cp -f /Users/irteamsu/scripts/macos-recover-sudoers/sudoers-backup /etc/sudoers
     echo "$datetime: $0: Done."
+    exit 0
 fi
